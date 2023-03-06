967f0d18c41b8d138168273bf13ef103481a9678
keginold philip <xkegix@users.noreply.github.com>
2023-03-06 19:40:41 +0000

    Added Express Route to POST a new Item 19:35 Front-end View to add a new Item 19:35 Express Route to PUT an updated Item: 19:36 Front-end View to edit an existing Item: 19:36 Backend (Node.js, Express, and Sequelize, added login/logout function for user. All models and routes created! Did some front end work to help start team production.

--- server/routes/items.js
+++ server/routes/items.js
@@ -17,4 +17,39 @@ router.get('/:id', async (req, res) => {
   }
 })
 
+router.post('/', async (req, res) => {
+    const { name, description, price, category, image } = req.body;
+    try {
+      const newItem = await Item.create({ name, description, price, category, image });
+      res.status(201).json(newItem);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  })
+
+  // update item thingy.
+router.put('/:id', async (req, res) => {
+    const itemId = req.params.id;
+    const { name, description, price, category, image } = req.body;
+    try {
+      const item = await Item.findByPk(itemId);
+      if (item) {
+        item.name = name;
+        item.description = description;
+        item.price = price;
+        item.category = category;
+        item.image = image;
+        await item.save();
+        res.status(200).json(item);
+      } else {
+        res.status(404).json({ message: 'Item not found' });
+      }
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  })
+
+
+
+
 module.exports = router;
