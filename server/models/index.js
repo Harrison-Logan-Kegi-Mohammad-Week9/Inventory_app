4652df6debf5a194d7e4690b417fe7d7862602a6
keginold philip <xkegix@users.noreply.github.com>
2023-03-07 10:03:42 +0000

    index.js model set up for items not sauces lol!

--- server/models/index.js
+++ server/models/index.js
@@ -1,12 +1,15 @@
-const {Sequelize} = require('sequelize')
-const {sequelize} = require('../db')
+const { Sequelize } = require('sequelize');
+const { sequelize } = require('../db');
 
-const Item = sequelize.define("items", {
+const Item = sequelize.define('Item', {
   name: Sequelize.STRING,
+  description: Sequelize.STRING,
+  price: Sequelize.FLOAT,
+  category: Sequelize.STRING,
   image: Sequelize.STRING,
-});
+})
 
 module.exports = {
   db: sequelize,
-  Sauce,
-};
+  Item,
+}
